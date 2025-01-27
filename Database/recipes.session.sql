424c38e057ea4b1d61f637aa330c22b835cc78e0
Ekabhat <ekabhat@bu.edu>
2025-01-27 18:04:28 -0500

    updated db tables

--- Database/recipes.session.sql
+++ Database/recipes.session.sql
@@ -1,4 +1,24 @@
 -- @block
-CREATE TABLE Users(
-    
+CREATE TABLE users(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    email varchar NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP
+)
+
+
+-- @block
+CREATE TABLE Recipes(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    title varchar NOT NULL,
+    description text NOT NULL,
+    instructions text NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
+);
+
+-- @block
+CREATE TABLE Ingredients(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    name varchar NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP
 );
