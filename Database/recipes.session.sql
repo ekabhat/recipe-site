0b86fe4ab2cad83cc727270be54024b46d303b7c
Ekabhat <ekabhat@bu.edu>
2025-01-27 18:17:16 -0500

    added user to ingredient and recipe to ingredient tables

--- Database/recipes.session.sql
+++ Database/recipes.session.sql
@@ -22,3 +22,24 @@ CREATE TABLE Ingredients(
     name varchar NOT NULL,
     created_at datetime DEFAULT CURRENT_TIMESTAMP
 );
+
+-- @block
+CREATE TABLE UserIngredients(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    user_id integer NOT NULL,
+    ingredient_id integer NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredients(id)
+);
+
+
+-- @block
+CREATE TABLE RecipeIngredients(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    recipe_id integer NOT NULL,
+    ingredient_id integer NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipe_id) REFERENCES Recipes(id),
+    FOREIGN KEY (ingredient_id) REFERENCES Ingredients(id)
+);
