d116f65dd1e6d6f08c92cfd87505452c4c24505e
ekabhat <ekabhat3748@gmail.com>
2025-02-11 21:50:55 -0500

    imports for app.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react"; // Chakra UI for styling
+import { BrowserRouter } from "react-router-dom"; // React Router for navigation
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
